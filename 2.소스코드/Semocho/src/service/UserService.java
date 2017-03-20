f8e5459f15d3a2bcb5040973b1ff1486965b880e
heajin <thkdld9732@naver.com>
2017-03-20 12:44:37 +0900

    fgasfsa

--- 2.소스코드/Semocho/src/service/UserService.java
+++ 2.소스코드/Semocho/src/service/UserService.java
@@ -1,5 +1,15 @@
 package service;
 
-public interface UserService {
+import java.util.List;
 
+import domain.User;
+
+public interface UserService {
+	void register (User user);
+	void selectId(String id);
+	List<User> findUserByName(String name);
+	User findUserById(String id);
+	User findUserAll();
+	void deleteUser(String id);
+	void updateUser(User user);
 }
