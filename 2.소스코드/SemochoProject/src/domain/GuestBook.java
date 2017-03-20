94096cc1807ca02c939116ae8005ae4e137c7ae0
이창훈 <lch3427@naver.com>
2017-03-20 12:04:02 +0900

    sdf

--- 2.소스코드/SemochoProject/src/domain/GuestBook.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public class GuestBook {
-	String id;
-	String writerId;
-	String content;
-
-}
--- 2.소스코드/SemochoProject/src/domain/Image.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public class Image {
-	String id;
-	String imageType;
-	String imgaeName;
-
-}
