5aa6e9f5ea2bb4889549f624650ae559998d9bd9
chikwang <chee0806@naver.com>
2017-03-20 12:51:56 +0900

    ckckck

--- 2.소스코드/Semocho/src/service/PostService.java
+++ 2.소스코드/Semocho/src/service/PostService.java
@@ -1,5 +1,30 @@
 package service;
 
+import java.util.List;
+
+import domain.Choice;
+import domain.Comment;
+import domain.Post;
+
 public interface PostService {
+	
+	List<Comment> searchChoice(Choice choice);
+	
+	void registerComment(Comment comment, String postId);
+	void removeComment(String commentId);
+	
+	List<Post> searchPostsByCategory(String Category);
+	List<Post> searchPostsByWriterId(String writerId);
+	Post searchPostById(String postId);
+	List<Post> searchPostsByTitle(String title);
+	List<Post> searchAllPosts();
+	void registerPost(Post post);
+	void removePost(String postId);
+	
+	List<Post> searchByAlaming(String userId);
+	void registerAlaming(String userId, String postId);
+	void removeAlaming(String userId, String postId);
+	
+	
 
 }
