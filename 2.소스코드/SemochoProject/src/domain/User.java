4c4328a582568fb88fc7ed2d3e52024f05eed80b
hyeongmoon choi <hyeungmoon@gmail.com>
2017-03-20 10:51:44 +0900

    domain

--- 2.소스코드/SemochoProject/src/domain/User.java
+++ 2.소스코드/SemochoProject/src/domain/User.java
@@ -1,5 +1,15 @@
 package domain;
 
 public class User {
+	String id;
+	String password;
+	String name;
+	String birth;
+	String tel;
+	String interest;
+	String email;
+	String gender;
+	String grade;
+	int semo;
 
 }
