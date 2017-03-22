5d9ccfa09762afd0d0c55c7b4579b51f5f0e2e54
teak <kimo343459@naver.com>
2017-03-22 11:15:51 +0900

    new

---
+++ 2.소스코드/0322합본/semo/src/semo/store/facade/GoodStore.java
@@ -0,0 +1,22 @@
+package semo.store.facade;
+
+import java.util.List;
+
+import semo.domain.Good;
+import semo.domain.User;
+
+public interface GoodStore {
+	
+	void insertGood(Good good);//
+	void deleteGood(String goodId);//
+	List<Good> selectAllGood();//
+	List<String> selectWinners(String GoodId);
+	
+	void insertEnterUsers(String userId, String goodId);
+	List<String> selectEnterUsers(String goodId);
+	void insertWinners(String userId , String goodId);
+	
+	void deleteWinnersGood(String gooId);
+	
+
+}
