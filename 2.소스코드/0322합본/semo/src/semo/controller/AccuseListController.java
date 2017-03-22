f11e1b42adc6925fea5e9075b4dca4c28a808843
teak <kimo343459@naver.com>
2017-03-22 16:59:19 +0900

    new

--- 2.소스코드/0322합본/semo/src/semo/controller/AccuseListController.java
+++ 2.소스코드/0322합본/semo/src/semo/controller/AccuseListController.java
@@ -11,10 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 public class AccuseListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 	
-	
+		
 		
 	
 	
