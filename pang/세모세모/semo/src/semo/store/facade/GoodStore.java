72b5d0612b266f47f5fed7f2e4229284aec1cc51
이창훈 <lch3427@naver.com>
2017-03-22 17:12:02 +0900

    sdf

---
+++ pang/세모세모/semo/src/semo/store/facade/GoodStore.java
@@ -0,0 +1,24 @@
+package semo.store.facade;
+
+import java.util.List;
+
+import semo.domain.Good;
+import semo.domain.User;
+
+public interface GoodStore {
+	
+	void insertGood(Good good);//
+	void deleteGood(String goodId);//
+	List<Good> selectAllGood();//
+	List<String> selectWinners(String GoodId);
+	
+	void insertEnterUsers(String userId, String goodId);
+	List<String> selectEnterUsers(String goodId);
+	void insertWinners(String userId , String goodId);
+	
+	void deleteWinnersGood(String gooId);
+	Good selectGoodByGoodId(String goodId);
+	List<String> selectGoodIdbyUserId(String userId);
+	
+
+}
