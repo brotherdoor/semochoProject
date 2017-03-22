59738a835684f1ba20da360cb5ef054870c9b5f9
chikwang <chee0806@naver.com>
2017-03-22 15:13:52 +0900

    add.ck.service

---
+++ chee/semo.ck.add.service/src/semo/store/logic/GoodStoreLogic.java
@@ -0,0 +1,169 @@
+package semo.store.logic;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import semo.domain.Good;
+import semo.domain.User;
+import semo.store.facade.GoodStore;
+import semo.store.mapper.GoodMapper;
+import semo.store.mapper.UserMapper;
+
+public class GoodStoreLogic implements GoodStore{
+	private SqlSessionFactory factory;
+	
+	public GoodStoreLogic() {
+		factory = SqlSessionFactoryProvider.getSqlSessionFactory();
+	}
+	@Override
+	public void insertGood(Good good) {//
+		SqlSession session = factory.openSession();
+		
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			mapper.insertGood(good);
+			session.commit();
+		}finally{
+			session.close();
+		}
+	}
+
+	@Override
+	public void deleteGood(String goodId) {//
+		SqlSession session = factory.openSession();
+		
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			mapper.deleteGood(goodId);
+			session.commit();
+		}finally{
+			session.close();
+		}
+	}
+
+	@Override
+	public List<Good> selectAllGood() {//
+		SqlSession session = factory.openSession();
+		List<Good> list = null;
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			list = mapper.selectAllGood();
+			session.commit();
+		}finally{
+			session.close();
+		}
+		return list;
+	}
+
+	@Override
+	public List<String> selectWinners(String GoodId) {
+		SqlSession session = factory.openSession();
+		List<String> list = null;
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			list = mapper.selectWinners(GoodId);
+			
+		}finally {
+			session.close();
+		}
+		return list;
+	}
+
+	@Override
+	public void insertEnterUsers(String userId, String goodId) { // 경품응모
+		SqlSession session = factory.openSession();
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("user", userId);
+		map.put("good", new Integer(Integer.parseInt(goodId)));
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			mapper.insertEnterUsers(map);
+			session.commit();
+		}finally{
+			session.close();
+		}
+	}
+
+	@Override
+	public List<String> selectEnterUsers(String goodId) {
+		SqlSession session = factory.openSession();
+		List<String> list = null;
+		
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			list = mapper.selectEnterUsers(goodId);
+			session.commit();
+		}finally{
+			session.close();
+		}
+		return list;
+	}
+	@Override
+	public void insertWinners(String userId, String goodId) {
+		
+		SqlSession session = factory.openSession();
+		HashMap<String, String> map = new HashMap<>();
+		map.put("userId", userId);
+		map.put("goodId", goodId);
+		
+		
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			mapper.insertWinners(map);
+			session.commit();
+		}finally{
+			session.close();
+		}
+
+		
+		
+		
+	}
+	@Override
+	public void deleteWinnersGood(String gooId) {
+		
+		SqlSession session = factory.openSession();
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			mapper.deleteWinnersGood(gooId);
+			
+		}finally{
+			session.close();
+		}
+		
+	}
+	
+	
+	@Override
+	public List<String> selectGoodIdbyUserId(String userId) {
+		SqlSession session = factory.openSession();
+		List<String> list = null;
+		
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			list = mapper.selectGoodIdbyUserId(userId);
+			session.commit();
+		}finally{
+			session.close();
+		}
+		return list;
+	}
+	@Override
+	public Good selectGoodByGoodId(String goodId) {
+		SqlSession session = factory.openSession();
+		Good good = null;
+		
+		try{
+			GoodMapper mapper = session.getMapper(GoodMapper.class);
+			good = mapper.selectGoodByGoodId(goodId);
+			session.commit();
+		}finally{
+			session.close();
+		}
+		return good;
+	}
+
+}
---
+++ chee/semo.ck.add.service/test/semo/test/ChoiceStoreLogicTest.java
@@ -0,0 +1,74 @@
+package semo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import semo.domain.Choice;
+import semo.domain.Image;
+import semo.store.logic.ChoiceStoreLogic;
+
+public class ChoiceStoreLogicTest {
+	
+	private ChoiceStoreLogic store;
+	
+	@Before
+	public void setUp(){
+		store = new  ChoiceStoreLogic();
+	}
+	
+	
+
+//	@Test
+//	public void testInsertChoice() {
+//		
+//		Choice choice = new Choice();
+//		Image image = new Image();
+//		
+//		image.setImageType("jpg");
+//		image.setImgaeName("test");
+//		
+//		choice.setContent("æ»≥Á«œººø‰");
+//		choice.setImage(image);
+//				
+//		
+//
+//		store.insertChoice(choice, "1");
+//	
+//	}
+//
+//	@Test
+//	public void testSelectChoice() {
+//		
+//		
+//		assertEquals(1, store.selectChoice("1").size());
+//
+//		
+//	
+//	}
+//
+//	@Test
+//	public void testSelectVoteUsers() {
+//		
+//		assertEquals(1, store.selectVoteUsers("1").size());
+//		
+//	}
+//
+//	@Test
+//	public void testInsertVoteUser() {
+//		
+//		Choice choice = store.selectChoice("1").get(0);
+//		
+//		store.insertVoteUser(choice, "22");
+//		assertEquals(1, store.selectVoteUsers("1").size());
+//	}
+//
+//	@Test
+//	public void testCheckVote() {
+//		
+//		
+//		assertEquals("1", store.checkVote("1", "33"));
+//	}
+
+}
