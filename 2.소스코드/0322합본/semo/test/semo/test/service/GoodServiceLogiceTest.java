b4286572413a133f31b342bf8c7168a73d9e47fd
hyeongmoon choi <hyeungmoon@gmail.com>
2017-03-23 09:26:40 +0900

    jj

---
+++ 2.소스코드/0322합본/semo/test/semo/test/service/GoodServiceLogiceTest.java
@@ -0,0 +1,24 @@
+package semo.test.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoodServiceLogiceTest {
+
+	@Test
+	public void testSearchAllGood() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterEnterUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectGoodByGoodId() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 2.소스코드/0322합본/semo/test/semo/test/service/UserServiceLogicTest.java
@@ -0,0 +1,64 @@
+package semo.test.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserServiceLogicTest {
+
+	@Test
+	public void testRegisterUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchUserByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchUserById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchUserAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifyUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchFriendById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchFriendAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchFriend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveFriend() {
+		fail("Not yet implemented");
+	}
+
+}
